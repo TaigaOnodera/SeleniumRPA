14d0c390df7cb06bfdd06fcca47cf8fdc1f16026
TaigaOnodera <o1710138@edu.cc.uec.ac.jp>
2020-04-18 19:22:45 +0900

    1時間おきの通知とか, エラーへの対処(?)とか

--- NotifySwitchPrice.py
+++ NotifySwitchPrice.py
@@ -4,6 +4,7 @@ import unittest, time, sys, re, requests
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
@@ -16,62 +17,84 @@ COLORFUL_URL = 'https://www.amazon.co.jp/Nintendo-Switch-%E3%83%8B%E3%83%B3%E3%8
 
 DOUMORI_URL = 'https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-Nintendo-Switch-%E3%81%82%E3%81%A4%E3%81%BE%E3%82%8C-%E3%81%A9%E3%81%86%E3%81%B6%E3%81%A4%E3%81%AE%E6%A3%AE%E3%82%BB%E3%83%83%E3%83%88/dp/B084HPMVNN/ref=sr_1_4?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&dchild=1&keywords=Nintendo+Switch+%E6%9C%AC%E4%BD%93+%28%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%29&qid=1586953706&s=videogames&sr=1-4'
 
-LIMIT_VALUE = 34000
+LIMIT_VALUE_NORMAL  = 34000
+LIMIT_VALUE_DOUMORI = 40000
 
 def recogPrice(webdriver,URL,lim_value):
-	# URLを開く
-	webdriver.get(URL)
-	# lim_valueとの比較用の変数 
-	# 0にしておけばとりあえず比較はしてくれるし、異常の通知になるかも？
-	test_value = 0;
+    # URLを開く
+    webdriver.get(URL)
+    # lim_valueとの比較用の変数 
+    # 0にしておけばとりあえず比較はしてくれるし、異常の通知になるかも？
+    test_value = 0;
 
-	# 値段を取得
-	try:
-		# 「カートに入れる」ボタンの場合
-		price = webdriver.find_element_by_id('buyNew_noncbb').text
-	except NoSuchElementException:
-		# 「すべての出品者を見る」ボタンの場合
-		price = webdriver.find_element_by_id('unqualifiedBuyBox').text
-	
-	# 空文字じゃなければ
-	if price:
-		price = re.search('￥[0-9\,]*', price).group()
-		test_value = int(price[1:].replace(",",""))
-	else:
-		price = price + "(取得できませんでした。)"
+    # 値段を取得
+    try:
+        # 「カートに入れる」ボタンの場合
+        price = webdriver.find_element_by_id('buyNew_noncbb').text
+    except NoSuchElementException:
+        # 「すべての出品者を見る」ボタンの場合
+        price = webdriver.find_element_by_id('unqualifiedBuyBox').text
+    except :
+        price = ""
 
-	# lim_value以下だったらLINEで送る
-	if test_value< lim_value:
-		# 商品名を取得
-		itemName = webdriver.find_element_by_id('productTitle').text
-		# 現在時刻も取得
-		now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-		# LINE Notifyで送るメッセージ
-		message = ("%s\n\n商品名 : %s\n\nURL: %s \n\n価格 : %s\n"%(now, itemName, URL, price))
-		payload = {'message': message}
-		
-		# 送信処理
-		headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
-		r = requests.post(LINE_Notify_URL, headers=headers, params=payload,)
-		#一応PCにも表示
-		print(message)
+    # 空文字じゃなければ
+    if price:
+        price = re.search('￥[0-9\,]*', price).group()
+        test_value = int(price[1:].replace(",",""))
+    else:
+        price = price + "(取得できませんでした。)"
+
+    compare_result = test_value < lim_value
+
+    # lim_value以下だったらLINEで送る
+    if compare_result:
+        # 商品名を取得
+        itemName = webdriver.find_element_by_id('productTitle').text
+        # 現在時刻も取得
+        now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        # LINE Notifyで送るメッセージ
+        message = ("%s\n\n商品名 : %s\n\nURL: %s \n\n価格 : %s\n"%(now, itemName, URL, price))
+        # 送信処理
+        payload = {'message': message}
+        headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
+        r = requests.post(LINE_Notify_URL, headers=headers, params=payload,)
+        #一応PCにも表示
+        #print(message)
+
+    return not compare_result
 
 if __name__ == '__main__':
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1280,1024')
+    # ブラウザの起動とログイン
+    try:
+        b = webdriver.Chrome(options=options,executable_path='/usr/local/bin/chromedriver')
+    except:
+        print('Failed to open browser.')
+        exit()
 
-	# ブラウザの起動とログイン
-	try:
-		b = webdriver.Chrome('/bin/chromedriver')
-	except:
-		l('Failed to open browser.')
-		exit()
+    # 今何分経ったかをカウントする変数
+    count = 0
+    # 値段確認ループ
+    while True:
+        # 値段確認
+        gray    = recogPrice(b,GRAY_URL    , LIMIT_VALUE_NORMAL )
+        color   = recogPrice(b,COLORFUL_URL, LIMIT_VALUE_NORMAL )
+        doumori = recogPrice(b,DOUMORI_URL , LIMIT_VALUE_DOUMORI)
 
-	# 値段確認ループ
-	while True:
-		# 値段確認
-		recogPrice(b,GRAY_URL, LIMIT_VALUE)
-		recogPrice(b,COLORFUL_URL, LIMIT_VALUE)
-		recogPrice(b,DOUMORI_URL, 40000)
+        # １時間で異常なければ送信
+        if gray and color and doumori and count %60 == 0:
+            # LINE Notifyで送るメッセージ
+            message = datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " : Nothing for alert"
+            # 送信処理
+            payload = {'message': message}
+            headers = {'Authorization': 'Bearer ' + ACCESS_TOKEN}
+            r = requests.post(LINE_Notify_URL, headers=headers, params=payload,)
+            count = 0
 
-		time.sleep(60)
-
+        count = count+1
+        time.sleep(60)
 	
